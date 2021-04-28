395748e8fe104cdb49a532df31a62eda5e1d8335
Guillestations <guillejuanmanuel@gmail.com>
2021-04-28 10:27:33 -0300

    deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const validarEmail = require('./models/validar')
 require('dotenv').config();
 
 const app = express();
-const PORT =  process.env.PORT || 8080;
+const PORT =  process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}))
 // app.use(bodyParser.urlencoded({ extended: false }))
